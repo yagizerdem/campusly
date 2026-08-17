f9a8e9cdd107ae00c23dc159a4afb0bfb702b44a
yagiz erdem <yagizerdem819@gmail.com>
2026-08-17 10:47:41 +0300

    env configuration added

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const { join } = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: [join(__dirname, ".env.dev"), join(__dirname, ".env.prod")],
+  override: true,
+});
+
+const { app } = require("./app");
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.info(`server up on port ${PORT}`);
+});
