8c2bcc93559ea2d43aa36c856aa7d1e73fbbaabc
yagiz erdem <yagizerdem819@gmail.com>
2026-08-18 03:16:49 +0300

    build(backend): add UUID dependency and Firebase admin token script

--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -1,8 +1,8 @@
-import { initializeApp, cert } from "firebase-admin/app";
+import { initializeApp, cert, type ServiceAccount } from "firebase-admin/app";
 import serviceAccount from "@/.firebase/firebase-adminsdk.json" with { type: "json" };
 
 const firebaseApp = initializeApp({
-  credential: cert(JSON.stringify(serviceAccount)),
+  credential: cert(serviceAccount as ServiceAccount),
 });
 
 export { firebaseApp };
