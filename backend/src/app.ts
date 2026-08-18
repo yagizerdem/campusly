c23832c40e0d263b6f4faa53f6a5a1c83e9975bf
yagiz erdem <yagizerdem819@gmail.com>
2026-08-18 12:07:16 +0300

    feat(backend): added global-error hanlder

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import authRoute from "@route/auth-route.js";
+import { globalErrorHandler } from "@src/global-error-handler.js";
 
 const app = express();
 
@@ -13,5 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoute);
+
+app.use(globalErrorHandler);
 
 export { app };
